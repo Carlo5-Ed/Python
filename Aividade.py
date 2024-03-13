937048bb9f0a012f38a9c9331db90406f90fc88a
Carlos <carlosteiixeiira15@gmail.com>
2024-03-13 11:28:40 -0300

    primeiro commit

---
+++ Aividade.py
@@ -0,0 +1,30 @@
+print("----------Responda com sim ou não sobre os personagens da liga da justiça----------------")
+
+resposta1=input("O personagem solta lazer pelos olhos? s/sim ou n/não:\n")
+if resposta1=="s":
+    print("É o Sperman")
+else:
+    resposta2=input("O personagem possui um laço da verdade? s/sim ou n/não:\n")
+    if resposta2=="s":
+        print("É a Diana")
+    else:
+        resposta3=input("O personagem possui asas? s/sim ou n/não:\n")
+        if resposta3=="s":
+            print("É a Shayera Hall")
+        else:
+            resposta4=input("O personagem é verde? s/sim ou n/não:\n")
+            if resposta4=="s":
+                print("É o Lanterna Verde")
+            else:
+                resposta5=input("O personagem é a noite? s/sim ou n/não:\n")
+                if resposta5=="s":
+                    print("É o Batman")
+                else:
+                    resposta6=input("O personagem possui super velocidade? s/sim ou n/não:\n")
+                    if resposta6=="s":
+                        print("É o Flash")
+                    else:
+                        resposta7=input("O personagem possui invisibilidade? s/sim ou n/não:\n")
+                        if resposta7=="s":
+                            print("É o Caçador de Marte")
+
---
+++ primeiros comandos.py
@@ -0,0 +1,6 @@
+print("Hellow World!!!")
+
+nome=input("Qual é o seu nome?")
+idade=input("Qual sua idade?")
+peso=input("Qual é o seu peso?")
+print(nome,idade,peso)
