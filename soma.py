1b336d8ffee9dc259cbc4e3810f8738accb3550c
Carlos <carlosteiixeiira15@gmail.com>
2024-03-14 11:09:05 -0300

    soma

---
+++ soma.py
@@ -0,0 +1,5 @@
+num=int(input("Primeiro número "))
+num2=int(input("Segundo número"))
+s=num+num2
+#print("A soma entre", num, "e", num2, "vale", s )
+print("A soma entre {} e {} vale {}".format(num,num2,s))
