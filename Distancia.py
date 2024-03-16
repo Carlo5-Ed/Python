d774298cc3096bf1419415c6fed216dd3fa52567
Carlos <carlosteiixeiira15@gmail.com>
2024-03-16 11:54:03 -0300

    distancia, notas e tabuada

---
+++ Distancia.py
@@ -0,0 +1,8 @@
+num=float(input("Digite uma distância em metros: "))
+km=num/1000
+hm=num/100
+am=num/10
+dm=num*10
+cm=num*100
+mm=num*1000
+print("{}km\n{}hm\n{}am\n{}dm\n{}cm\n{}mm".format(km, hm, am, dm, cm, mm))
