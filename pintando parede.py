5b9795b0c7c135a8404d199f5c7c7de4a36520a6
Carlos <carlosteiixeiira15@gmail.com>
2024-03-17 12:29:18 -0300

    pintando parede

---
+++ pintando parede.py
@@ -0,0 +1,6 @@
+larg=float(input("Largura da parede: "))
+altu=float(input("Altura da parede: "))
+area= larg*altu
+print("Parede tem a dimensão de {}x{} e sua área de {}m². ".format(larg, altu, area))
+tinta=area/2
+print("Litros de tinta para pintar a parede é de {}l de tinta".format(tinta))
